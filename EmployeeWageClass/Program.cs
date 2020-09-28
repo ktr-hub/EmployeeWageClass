3932ce501c2e2d3966eb35d11d08a4bba16b3ec9
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 09:10:30 +0530

    [Tirupathi] Add. first master commit

---
+++ EmployeeWageClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee portal!");
+        }
+    }
+}
